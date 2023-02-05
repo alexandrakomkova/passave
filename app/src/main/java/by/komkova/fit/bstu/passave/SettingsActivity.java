55b5c8deb322b9966f90fa2640a27a706f9f74af
sasha <sashakomkova2001@gmail.com>
2023-02-05 19:13:45 +0300

    3 screen design

---
+++ app/src/main/java/by/komkova/fit/bstu/passave/SettingsActivity.java
@@ -0,0 +1,14 @@
+package by.komkova.fit.bstu.passave;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+    }
+}
