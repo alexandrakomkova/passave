120c1327e62366f2911647c55c99caf105c3450f
sasha <sashakomkova2001@gmail.com>
2023-02-26 20:32:48 +0300

    add generate password screen

---
+++ app/src/main/java/by/komkova/fit/bstu/passave/GeneratePasswordActivity.java
@@ -0,0 +1,14 @@
+package by.komkova.fit.bstu.passave;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GeneratePasswordActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generate_password);
+    }
+}
