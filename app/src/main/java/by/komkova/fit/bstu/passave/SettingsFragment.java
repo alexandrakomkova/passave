5d7b54bb99a5c5f9e0d160ed3ee65b91823e3ba9
sasha <sashakomkova2001@gmail.com>
2023-03-22 12:47:59 +0300

    add settings fragment add navigation from settings to security fragment make imagebutton match_parent by width

--- app/src/main/java/by/komkova/fit/bstu/passave/SettingsFragment.java
+++ app/src/main/java/by/komkova/fit/bstu/passave/SettingsFragment.java
@@ -3,17 +3,41 @@ package by.komkova.fit.bstu.passave;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements View.OnClickListener {
+
+    ImageButton security_ibtn;
+    final String log_tag = getClass().getName();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        security_ibtn = view.findViewById(R.id.security_btn);
+        security_ibtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction fragmentTransaction = getActivity()
+                        .getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_layout, new SecurityFragment());
+                fragmentTransaction.commit();
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
