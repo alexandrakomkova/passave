63b6b98b4391018984e249ce0b2f05bbc28f476e
sasha <sashakomkova2001@gmail.com>
2023-03-22 23:39:12 +0300

    add RCAdapter and RCModel

---
+++ app/src/main/java/by/komkova/fit/bstu/passave/RCModel.java
@@ -0,0 +1,12 @@
+package by.komkova.fit.bstu.passave;
+
+public class RCModel {
+
+    String title;
+    int image;
+
+    public RCModel(String title, int image){
+        this.title = title;
+        this.image = image;
+    }
+}
