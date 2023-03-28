aa327e598d0b396098db852d63ece722ab9c18ce
sasha <sashakomkova2001@gmail.com>
2023-03-28 10:47:52 +0300

    add fragment_details.xml layout

---
+++ app/src/main/java/by/komkova/fit/bstu/passave/DetailsFragment.java
@@ -0,0 +1,30 @@
+package by.komkova.fit.bstu.passave;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DetailsFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_details, container, false);
+
+        return view;
+    }
+}
