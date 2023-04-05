697e4946d554f2aedf62e795ddbf3f59ba417510
sasha <sashakomkova2001@gmail.com>
2023-04-05 12:52:52 +0300

    add SecurityProvider

--- app/src/main/java/by/komkova/fit/bstu/passave/SecurityProvider.java
+++ app/src/main/java/by/komkova/fit/bstu/passave/SecurityProvider.java
@@ -13,34 +13,34 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-public class TagProvider extends ContentProvider {
-    static final String log_tag = TagProvider.class.getName();
+public class SecurityProvider extends ContentProvider {
+    static final String log_tag = SecurityProvider.class.getName();
     DatabaseHelper databaseHelper;
     SQLiteDatabase db;
 
-    static final String AUTHORITY = "by.komkova.fit.bstu.passave.providers.TagList";
+    static final String AUTHORITY = "by.komkova.fit.bstu.passave.providers.SecurityList";
     static final String PATH = "list";
-    public static final Uri TAG_URI = Uri.parse("content://" + AUTHORITY + "/"+
+    public static final Uri SECURITY_URI = Uri.parse("content://" + AUTHORITY + "/"+
             PATH);
 
-    static final String TAG_LIST_TYPE = "vnd.android.cursor.dir/vnd."+
+    static final String SECURITY_LIST_TYPE = "vnd.android.cursor.dir/vnd."+
             AUTHORITY+"."+PATH;
-    static final String TAG_TYPE = "vnd.android.cursor.item/vnd."+
+    static final String SECURITY_TYPE = "vnd.android.cursor.item/vnd."+
             AUTHORITY+"."+PATH;
-    static final int URI_TAGS = 1;
-    static final int URI_TAG_ID = 2;
+    static final int URI_SECURITIES = 1;
+    static final int URI_SECURITY_ID = 2;
 
     private static UriMatcher uriMathcher;
 
-    static{
+    static {
         uriMathcher = new UriMatcher(UriMatcher.NO_MATCH);
-        uriMathcher.addURI(AUTHORITY, PATH, URI_TAGS);
-        uriMathcher.addURI(AUTHORITY, PATH + "/#", URI_TAG_ID);
+        uriMathcher.addURI(AUTHORITY, PATH, URI_SECURITIES);
+        uriMathcher.addURI(AUTHORITY, PATH + "/#", URI_SECURITY_ID);
     }
 
     @Override
     public boolean onCreate() {
-        Log.d(log_tag, "TagProvider onCreate()");
+        Log.d(log_tag, "SecurityProvider onCreate()");
         databaseHelper = new DatabaseHelper(getContext());
         return true;
     }
@@ -49,28 +49,28 @@ public class TagProvider extends ContentProvider {
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
         switch (uriMathcher.match(uri)){
-            case URI_TAGS:
-                Log.d(log_tag, "URI_TAGS");
+            case URI_SECURITIES:
+                Log.d(log_tag, "URI_SECURITIES");
                 if(TextUtils.isEmpty(sortOrder)){
-                    sortOrder = DatabaseHelper.TAG_COLUMN_TAG_NAME + " ASC";
+                    sortOrder = DatabaseHelper.SECURITY_COLUMN_ALGORITHM_NAME + " ASC";
                 }
                 break;
-            case URI_TAG_ID:
+            case URI_SECURITY_ID:
                 String id = uri.getLastPathSegment();
-                Log.d(log_tag, "URI_TAG_ID = "+ id);
+                Log.d(log_tag, "URI_SECURITY_ID = "+ id);
                 if(TextUtils.isEmpty(selection)){
-                    selection = selection + " AND " + DatabaseHelper.TAG_COLUMN_ID + " = " + id;
+                    selection = selection + " AND " + DatabaseHelper.SECURITY_COLUMN_ID + " = " + id;
                 }
                 break;
             default:
                 throw new IllegalArgumentException("wrong URI: " + uri);
         }
         db = databaseHelper.getWritableDatabase();
-        Cursor cursor = db.query(DatabaseHelper.TAG_TABLE, projection, selection,
+        Cursor cursor = db.query(DatabaseHelper.SECURITY_TABLE, projection, selection,
                 selectionArgs, null, null, sortOrder);
 
         cursor.setNotificationUri(getContext().getContentResolver(),
-                TAG_URI);
+                SECURITY_URI);
         Log.d(log_tag, "query completed, "+ uri.toString());
 
         return cursor;
@@ -80,10 +80,10 @@ public class TagProvider extends ContentProvider {
     @Override
     public String getType(@NonNull Uri uri) {
         switch(uriMathcher.match(uri)){
-            case URI_TAG_ID:
-                return TAG_TYPE;
-            case URI_TAGS:
-                return TAG_LIST_TYPE;
+            case URI_SECURITY_ID:
+                return SECURITY_TYPE;
+            case URI_SECURITIES:
+                return SECURITY_LIST_TYPE;
         }
         Log.d(log_tag, "getType, "+ uri.toString());
         return null;
@@ -92,12 +92,12 @@ public class TagProvider extends ContentProvider {
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
-        if(uriMathcher.match(uri) != URI_TAGS){
+        if(uriMathcher.match(uri) != URI_SECURITIES){
             throw new IllegalArgumentException("wrong uri: "+uri);
         }
         db = databaseHelper.getWritableDatabase();
-        long rowID = db.insert(databaseHelper.TAG_TABLE, null, contentValues);
-        Uri result = ContentUris.withAppendedId(TAG_URI, rowID);
+        long rowID = db.insert(DatabaseHelper.SECURITY_TABLE, null, contentValues);
+        Uri result = ContentUris.withAppendedId(SECURITY_URI, rowID);
         getContext().getContentResolver().notifyChange(result, null);
         Log.d(log_tag, "insert completed, "+ uri.toString());
 
@@ -107,18 +107,18 @@ public class TagProvider extends ContentProvider {
     @Override
     public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
         switch (uriMathcher.match(uri)){
-            case URI_TAGS:
-                Log.d(log_tag, "URI_TAGS");
+            case URI_SECURITIES:
+                Log.d(log_tag, "URI_SECURITIES");
 
                 break;
-            case URI_TAG_ID:
+            case URI_SECURITY_ID:
                 String id = uri.getLastPathSegment();
-                Log.d(log_tag, "URI_TAG_ID = "+ id);
+                Log.d(log_tag, "URI_SECURITY_ID = "+ id);
                 if(TextUtils.isEmpty(selection)){
-                    selection =  DatabaseHelper.TAG_COLUMN_ID + " = " + id;
+                    selection =  DatabaseHelper.SECURITY_COLUMN_ID + " = " + id;
                 }
                 else{
-                    selection = selection + " AND " + DatabaseHelper.TAG_COLUMN_ID + " = " + id;
+                    selection = selection + " AND " + DatabaseHelper.SECURITY_COLUMN_ID + " = " + id;
                 }
                 break;
             default:
@@ -126,7 +126,7 @@ public class TagProvider extends ContentProvider {
         }
         db = databaseHelper.getWritableDatabase();
 
-        int rowCount = db.delete(DatabaseHelper.TAG_TABLE, selection, selectionArgs);
+        int rowCount = db.delete(DatabaseHelper.SECURITY_TABLE, selection, selectionArgs);
 
         getContext().getContentResolver().notifyChange(uri, null);
         Log.d(log_tag, "delete completed, "+ uri.toString());
@@ -137,17 +137,17 @@ public class TagProvider extends ContentProvider {
     @Override
     public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
         switch (uriMathcher.match(uri)){
-            case URI_TAGS:
-                Log.d(log_tag, "URI_TAGS");
+            case URI_SECURITIES:
+                Log.d(log_tag, "URI_SECURITIES");
                 break;
-            case URI_TAG_ID:
+            case URI_SECURITY_ID:
                 String id = uri.getLastPathSegment();
-                Log.d(log_tag, "URI_TAG_ID = "+ id);
+                Log.d(log_tag, "URI_SECURITY_ID = "+ id);
                 if(TextUtils.isEmpty(selection)){
-                    selection =  DatabaseHelper.TAG_COLUMN_ID + " = " + id;
+                    selection =  DatabaseHelper.SECURITY_COLUMN_ID + " = " + id;
                 }
                 else{
-                    selection = selection + " AND " + DatabaseHelper.TAG_COLUMN_ID + " = " + id;
+                    selection = selection + " AND " + DatabaseHelper.SECURITY_COLUMN_ID + " = " + id;
                 }
                 break;
             default:
@@ -155,7 +155,7 @@ public class TagProvider extends ContentProvider {
         }
         db = databaseHelper.getWritableDatabase();
 
-        int rowCount = db.update(DatabaseHelper.TAG_TABLE, values, selection, selectionArgs);
+        int rowCount = db.update(DatabaseHelper.SECURITY_TABLE, values, selection, selectionArgs);
 
         getContext().getContentResolver().notifyChange(uri, null);
         Log.d(log_tag, "updated, "+ uri.toString());
