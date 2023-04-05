d6f4ddf1c53a76755e3939b9fc158d6dc1bde8b5
sasha <sashakomkova2001@gmail.com>
2023-04-05 12:40:59 +0300

    add TagProvider

--- app/src/main/java/by/komkova/fit/bstu/passave/TagProvider.java
+++ app/src/main/java/by/komkova/fit/bstu/passave/TagProvider.java
@@ -13,34 +13,34 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-public class FolderProvider extends ContentProvider {
+public class TagProvider extends ContentProvider {
     static final String log_tag = FolderProvider.class.getName();
     DatabaseHelper databaseHelper;
     SQLiteDatabase db;
 
-    static final String AUTHORITY = "by.komkova.fit.bstu.passave.providers.FolderList";
+    static final String AUTHORITY = "by.komkova.fit.bstu.passave.providers.TagList";
     static final String PATH = "list";
-    public static final Uri FOLDER_URI = Uri.parse("content://" + AUTHORITY + "/"+
+    public static final Uri TAG_URI = Uri.parse("content://" + AUTHORITY + "/"+
             PATH);
 
-    static final String FOLDER_LIST_TYPE = "vnd.android.cursor.dir/vnd."+
+    static final String TAG_LIST_TYPE = "vnd.android.cursor.dir/vnd."+
             AUTHORITY+"."+PATH;
-    static final String FOLDER_TYPE = "vnd.android.cursor.item/vnd."+
+    static final String TAG_TYPE = "vnd.android.cursor.item/vnd."+
             AUTHORITY+"."+PATH;
-    static final int URI_FOLDERS = 1;
-    static final int URI_FOLDER_ID = 2;
+    static final int URI_TAGS = 1;
+    static final int URI_TAG_ID = 2;
 
     private static UriMatcher uriMathcher;
 
     static{
         uriMathcher = new UriMatcher(UriMatcher.NO_MATCH);
-        uriMathcher.addURI(AUTHORITY, PATH, URI_FOLDERS);
-        uriMathcher.addURI(AUTHORITY, PATH + "/#", URI_FOLDER_ID);
+        uriMathcher.addURI(AUTHORITY, PATH, URI_TAGS);
+        uriMathcher.addURI(AUTHORITY, PATH + "/#", URI_TAG_ID);
     }
 
     @Override
     public boolean onCreate() {
-        Log.d(log_tag, "FolderProvider onCreate()");
+        Log.d(log_tag, "TagProvider onCreate()");
         databaseHelper = new DatabaseHelper(getContext());
         return true;
     }
@@ -49,28 +49,28 @@ public class FolderProvider extends ContentProvider {
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
         switch (uriMathcher.match(uri)){
-            case URI_FOLDERS:
-                Log.d(log_tag, "URI_FOLDERS");
+            case URI_TAGS:
+                Log.d(log_tag, "URI_TAGS");
                 if(TextUtils.isEmpty(sortOrder)){
-                    sortOrder = DatabaseHelper.FOLDER_COLUMN_FOLDER_NAME + " ASC";
+                    sortOrder = DatabaseHelper.TAG_COLUMN_TAG_NAME + " ASC";
                 }
                 break;
-            case URI_FOLDER_ID:
+            case URI_TAG_ID:
                 String id = uri.getLastPathSegment();
-                Log.d(log_tag, "URI_FOLDER_ID = "+ id);
+                Log.d(log_tag, "URI_TAG_ID = "+ id);
                 if(TextUtils.isEmpty(selection)){
-                    selection = selection + " AND " +DatabaseHelper.FOLDER_COLUMN_ID + " = " + id;
+                    selection = selection + " AND " + DatabaseHelper.TAG_COLUMN_ID + " = " + id;
                 }
                 break;
             default:
                 throw new IllegalArgumentException("wrong URI: " + uri);
         }
         db = databaseHelper.getWritableDatabase();
-        Cursor cursor = db.query(DatabaseHelper.FOLDER_TABLE, projection, selection,
+        Cursor cursor = db.query(DatabaseHelper.TAG_TABLE, projection, selection,
                 selectionArgs, null, null, sortOrder);
 
         cursor.setNotificationUri(getContext().getContentResolver(),
-                FOLDER_URI);
+                TAG_URI);
         Log.d(log_tag, "query completed, "+ uri.toString());
 
         return cursor;
@@ -80,10 +80,10 @@ public class FolderProvider extends ContentProvider {
     @Override
     public String getType(@NonNull Uri uri) {
         switch(uriMathcher.match(uri)){
-            case URI_FOLDER_ID:
-                return FOLDER_TYPE;
-            case URI_FOLDERS:
-                return FOLDER_LIST_TYPE;
+            case URI_TAG_ID:
+                return TAG_TYPE;
+            case URI_TAGS:
+                return TAG_LIST_TYPE;
         }
         Log.d(log_tag, "getType, "+ uri.toString());
         return null;
@@ -92,12 +92,12 @@ public class FolderProvider extends ContentProvider {
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
-        if(uriMathcher.match(uri) != URI_FOLDERS){
+        if(uriMathcher.match(uri) != URI_TAGS){
             throw new IllegalArgumentException("wrong uri: "+uri);
         }
         db = databaseHelper.getWritableDatabase();
-        long rowID = db.insert(databaseHelper.FOLDER_TABLE, null, contentValues);
-        Uri result = ContentUris.withAppendedId(FOLDER_URI, rowID);
+        long rowID = db.insert(databaseHelper.TAG_TABLE, null, contentValues);
+        Uri result = ContentUris.withAppendedId(TAG_URI, rowID);
         getContext().getContentResolver().notifyChange(result, null);
         Log.d(log_tag, "insert completed, "+ uri.toString());
 
@@ -107,18 +107,18 @@ public class FolderProvider extends ContentProvider {
     @Override
     public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
         switch (uriMathcher.match(uri)){
-            case URI_FOLDERS:
-                Log.d(log_tag, "URI_FOLDERS");
+            case URI_TAGS:
+                Log.d(log_tag, "URI_TAGS");
 
                 break;
-            case URI_FOLDER_ID:
+            case URI_TAG_ID:
                 String id = uri.getLastPathSegment();
-                Log.d(log_tag, "URI_FOLDER_ID = "+ id);
+                Log.d(log_tag, "URI_TAG_ID = "+ id);
                 if(TextUtils.isEmpty(selection)){
-                    selection =  databaseHelper.FOLDER_COLUMN_ID + " = " + id;
+                    selection =  DatabaseHelper.TAG_COLUMN_ID + " = " + id;
                 }
                 else{
-                    selection = selection + " AND " +databaseHelper.FOLDER_COLUMN_ID + " = " + id;
+                    selection = selection + " AND " + DatabaseHelper.TAG_COLUMN_ID + " = " + id;
                 }
                 break;
             default:
@@ -126,7 +126,7 @@ public class FolderProvider extends ContentProvider {
         }
         db = databaseHelper.getWritableDatabase();
 
-        int rowCount = db.delete(databaseHelper.FOLDER_TABLE, selection, selectionArgs);
+        int rowCount = db.delete(DatabaseHelper.TAG_TABLE, selection, selectionArgs);
 
         getContext().getContentResolver().notifyChange(uri, null);
         Log.d(log_tag, "delete completed, "+ uri.toString());
@@ -137,17 +137,17 @@ public class FolderProvider extends ContentProvider {
     @Override
     public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
         switch (uriMathcher.match(uri)){
-            case URI_FOLDERS:
-                Log.d(log_tag, "URI_FOLDERS");
+            case URI_TAGS:
+                Log.d(log_tag, "URI_TAGS");
                 break;
-            case URI_FOLDER_ID:
+            case URI_TAG_ID:
                 String id = uri.getLastPathSegment();
-                Log.d(log_tag, "URI_FOLDER_ID = "+ id);
+                Log.d(log_tag, "URI_TAG_ID = "+ id);
                 if(TextUtils.isEmpty(selection)){
-                    selection =  databaseHelper.FOLDER_COLUMN_ID + " = " + id;
+                    selection =  DatabaseHelper.TAG_COLUMN_ID + " = " + id;
                 }
                 else{
-                    selection = selection + " AND " +databaseHelper.FOLDER_COLUMN_ID + " = " + id;
+                    selection = selection + " AND " + DatabaseHelper.TAG_COLUMN_ID + " = " + id;
                 }
                 break;
             default:
@@ -155,7 +155,7 @@ public class FolderProvider extends ContentProvider {
         }
         db = databaseHelper.getWritableDatabase();
 
-        int rowCount = db.update(databaseHelper.FOLDER_TABLE, values, selection, selectionArgs);
+        int rowCount = db.update(DatabaseHelper.TAG_TABLE, values, selection, selectionArgs);
 
         getContext().getContentResolver().notifyChange(uri, null);
         Log.d(log_tag, "updated, "+ uri.toString());
